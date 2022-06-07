0802ae9cf008a278f43be53374636a9f78e65b13
lijunjun <gionchat@gmail.com>
2022-06-07 13:31:35 +0800

    1、添加动态权限申请

--- app/src/main/java/me/relex/camerafilter/MainActivity.java
+++ app/src/main/java/me/relex/camerafilter/MainActivity.java
@@ -1,11 +1,18 @@
 package me.relex.camerafilter;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private int REQUEST_CODE_PERMISSIONS = 10;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +23,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.image_filter:
-                startActivity(new Intent(this, ImageFilterActivity.class));
 
-                break;
-            case R.id.video_record:
-                startActivity(new Intent(this, VideoRecordActivity.class));
-                break;
+        if (allPermissionsGranted()) {
+            switch (v.getId()) {
+                case R.id.image_filter:
+                    startActivity(new Intent(this, ImageFilterActivity.class));
+
+                    break;
+                case R.id.video_record:
+                    startActivity(new Intent(this, VideoRecordActivity.class));
+                    break;
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
+
+    }
+
+
+    private boolean allPermissionsGranted() {
+        //check if req permissions have been granted
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
